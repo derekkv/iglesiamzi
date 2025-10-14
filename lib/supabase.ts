b67e990c68e3b4fe62b21dd0157d32cd5ad80162
derekkv <derekisman712@gmail.com>
2025-10-14 11:37:05 -0500

    v1.0.2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "http://31.97.99.102:8000"
+const supabaseUrl = "https://backiglesia.mzipet.com"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTc2MDEzMDkzNCwiZXhwIjoyMDc1NzA2OTM0LCJpc3MiOiJzdXBhYmFzZS1kZW1vIn0.naTzXPohWMYyFR-3AjOty4U6pIi-CiD7KIbngMCyHDE"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
